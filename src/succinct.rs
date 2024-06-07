f3c56076cb86f862d7717d33b683a973e5b57bfd
Xinding Wei <weixinding@gmail.com>
2024-06-07 11:32:35 -0400

    Comment out syscall

--- src/succinct.rs
+++ src/succinct.rs
@@ -4,7 +4,7 @@ extern "C" {
 
 #[inline]
 pub fn keccakf(state: &mut [u64; 25]) {
-    unsafe {
-        syscall_keccak_permute(state.as_mut_ptr());
-    }
+    // unsafe {
+    //     syscall_keccak_permute(state.as_mut_ptr());
+    // }
 }
